253c93ca2af2698a8fe3f3f0a3cc266ed8b317dc
YPUN <guo88882@gmail.com>
2020-04-09 16:21:41 +0800

    Index頁面加入Vue.js

---
+++ MVC_TEST/Scripts/Home/Index.js
@@ -0,0 +1,18 @@
+﻿vmStart();
+var vm;
+function vmStart() {
+    vm = new Vue({
+        el: '#Index',
+        data: {
+           TestStr:"我在測試",
+        },
+        mounted: function () {
+            this.$nextTick(function () {
+              
+            });
+        },
+        methods: {
+         
+        }
+    })
+}
